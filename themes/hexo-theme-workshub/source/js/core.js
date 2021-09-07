6cdf0a85e61164b5c4a75e1f0e98fc9317d28b03
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-09-07 22:57:16 +0800

    theme

---
+++ themes/hexo-theme-workshub/source/js/core.js
@@ -0,0 +1,8 @@
+function search() {
+	window.open(
+		document.getElementById("search_form").getAttribute("action_e") +
+			" " +
+			document.getElementById("search_value").value
+	);
+	return false;
+}
